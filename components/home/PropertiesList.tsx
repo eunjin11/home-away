69eb4d31673ad6e0efdcebc4b56f388ccf7c0884
eunjin11 <whdmswls9307@gmail.com>
2024-08-14 17:48:23 +0900

    setup homepage

---
+++ components/home/PropertiesList.tsx
@@ -0,0 +1,7 @@
+import { PropertyCardProps } from "@/utils/types";
+
+function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
+  return <div></div>;
+}
+
+export default PropertiesList;
