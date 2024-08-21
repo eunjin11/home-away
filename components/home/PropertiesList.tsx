341dc80dac1917411e74989f6a8966a78361e02d
eunjin11 <whdmswls9307@gmail.com>
2024-08-21 16:47:24 +0900

    add property Cards, loading

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -1,7 +1,13 @@
-import { PropertyCardProps } from "@/utils/types";
+import PropertyCard from "../card/PropertyCard";
+import type { PropertyCardProps } from "@/utils/types";
 
 function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
-  return <div></div>;
+  return (
+    <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
+      {properties.map((property) => {
+        return <PropertyCard key={property.id} property={property} />;
+      })}
+    </section>
+  );
 }
-
 export default PropertiesList;
