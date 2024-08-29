e9853ccb83577f82e3c483b76e106999712d6cc6
eunjin11 <whdmswls9207@gmail.com>
2024-08-29 16:47:02 +0900

    add favorite page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,4 +1,14 @@
-function FavoritePage() {
-  return <h1 className="text-3xl">CheckoutPage</h1>;
+import EmptyList from "@/components/home/EmptyList";
+import PropertiesList from "@/components/home/PropertiesList";
+import { fetchFavorites } from "@/utils/actions";
+
+async function FavoritesPage() {
+  const favorites = await fetchFavorites();
+
+  if (favorites.length === 0) {
+    return <EmptyList />;
+  }
+
+  return <PropertiesList properties={favorites} />;
 }
-export default FavoritePage;
+export default FavoritesPage;
