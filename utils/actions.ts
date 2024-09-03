a62cf709243544ab4a74f59c7aeb53e30b14d911
eunjin11 <whdmswls9207@gmail.com>
2024-09-03 17:56:05 +0900

    add Rating Input

--- utils/actions.ts
+++ utils/actions.ts
@@ -12,6 +12,22 @@ import {
 } from "./schemas";
 import { uploadImage } from "./supabase";
 
+export const createReviewAction = async () => {
+  return { message: "create review" };
+};
+
+export const fetchPropertyReviews = async () => {
+  return { message: "fetch reviews" };
+};
+
+export const fetchPropertyReviewsByUser = async () => {
+  return { message: "fetch user reviews" };
+};
+
+export const deleteReviewAction = async () => {
+  return { message: "delete  reviews" };
+};
+
 export const fetchPropertyDetails = (id: string) => {
   return db.property.findUnique({
     where: {
