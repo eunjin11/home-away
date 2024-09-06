abcbe96153929b13ffa0002b31d21098810ecd69
eunjin11 <whdmswls9207@gmail.com>
2024-09-06 11:47:37 +0900

    add booking Page

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -14,10 +14,30 @@ import {
 
 function BookingCalendar() {
   const currentDate = new Date();
-
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+  const bookings = useProperty((state) => state.bookings);
+
+  const { toast } = useToast();
+
+  const blockedPeriods = generateBlockedPeriods({
+    bookings,
+    today: currentDate,
+  });
+
+  const unavailableDates = generateDisabledDates(blockedPeriods);
 
   useEffect(() => {
+    const selectedRange = generateDateRange(range);
+    const isDisabledDateIncluded = selectedRange.some((date) => {
+      if (unavailableDates[date]) {
+        setRange(defaultSelected);
+        toast({
+          description: "Some dates are booked. Please select again.",
+        });
+        return true;
+      }
+      return false;
+    });
     useProperty.setState({ range });
   }, [range]);
 
@@ -28,6 +48,7 @@ function BookingCalendar() {
       selected={range}
       onSelect={setRange}
       className="mb-4"
+      disabled={blockedPeriods}
     />
   );
 }
--- utils/format.ts
+++ utils/format.ts
@@ -11,3 +11,11 @@ export const formatCurrency = (amount: number | null) => {
     maximumFractionDigits: 0,
   }).format(value);
 };
+
+export const formatDate = (date: Date) => {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date);
+};
